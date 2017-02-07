298d0ad7e80b784dd2240ebc48218fe0f5008ca1
Phuttha Kusolkumbot <phuttha.kusolkumbot@thomsonreuters.com>
2017-02-07 13:55:58 +0700

    Change to node v6.9.5 + fix the schedule validation

--- test/lib/bricks/businesslogics/scenarios/helpers/schedule.validate.test.js
+++ test/lib/bricks/businesslogics/scenarios/helpers/schedule.validate.test.js
@@ -5,7 +5,9 @@ const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
 chai.use(chaiAsPromised);
 const expect = chai.expect;
+const mockrequire = require('mock-require');
 const ObjectID = require('bson').ObjectID;
+const sinon = require('sinon');
 const nodepath = require('path');
 
 const Logger = require('cta-logger');
@@ -40,32 +42,113 @@ const DEFAULTCEMENTHELPER = {
  */
 
 describe('Validate in scenarios schedule helper', () => {
-  const mockId = new ObjectID();
   let helper;
-  const DEFAULTINPUTJOB = {
-    nature: {
-      type: DEFAULTTYPE,
-      quality: 'update',
-    },
-    payload: {
-      id: mockId.toString(),
-      bar: 'barupdated',
-    },
-  };
-
   before(() => {
+    const dataModelPath = nodepath.join(appRootPath,
+      '/lib/utils/datamodels/', `${DEFAULTTYPE}.js`);
+    mockrequire(dataModelPath, sinon.stub());
     helper = new Helper(DEFAULTCEMENTHELPER, DEFAULTLOGGER, DEFAULTTYPE, DEFAULTAPIURLS);
   });
 
   context('when payload is valid', () => {
-    const mockInputContext = new Context(DEFAULTCEMENTHELPER, DEFAULTINPUTJOB);
-    let promise;
-    before(() => {
-      promise = helper._validate(mockInputContext);
+    context('action is initial', () => {
+
+      const initial = {
+        nature: {
+          type: DEFAULTTYPE,
+          quality: 'schedule',
+        },
+        payload: {
+          action: 'initial',
+        },
+      };
+
+      const mockInputContext = new Context(DEFAULTCEMENTHELPER, initial);
+      let promise;
+      before(() => {
+        promise = helper._validate(mockInputContext);
+      });
+
+      it('should resolve with ok', function() {
+        return expect(promise).to.eventually.have.property('ok', 1);
+      });
     });
 
-    it('should resolve with ok', function() {
-      return expect(promise).to.eventually.have.property('ok', 1);
+
+    context('action is schedule with an id', () => {
+      const schedule = {
+        nature: {
+          type: DEFAULTTYPE,
+          quality: 'schedule',
+        },
+        payload: {
+          action: 'schedule',
+          id: new ObjectID().toString(),
+        },
+      };
+
+      const mockInputContext = new Context(DEFAULTCEMENTHELPER, schedule);
+      let promise;
+      before(() => {
+        promise = helper._validate(mockInputContext);
+      });
+
+      it('should resolve with ok', function() {
+        return expect(promise).to.eventually.have.property('ok', 1);
+      });
     });
+
   });
+
+  context('when payload is invalid', () => {
+    context('when action is incorrect', () => {
+      const foo = {
+        nature: {
+          type: DEFAULTTYPE,
+          quality: 'schedule',
+        },
+        payload: {
+          action: 'foo',
+        },
+      };
+
+      const mockInputContext = new Context(DEFAULTCEMENTHELPER, foo);
+      let promise;
+      before(() => {
+        promise = helper._validate(mockInputContext);
+      });
+
+      it('should reject with error', () => {
+        return expect(promise).to.eventually
+          .be.rejectedWith(Error, `missing/incorrect \'action: ${foo.payload.action}\' in job payload `);
+
+      })
+    })
+
+
+    context('when id is missing', () => {
+      const foo = {
+        nature: {
+          type: DEFAULTTYPE,
+          quality: 'schedule',
+        },
+        payload: {
+          action: 'schedule',
+        },
+      };
+
+      const mockInputContext = new Context(DEFAULTCEMENTHELPER, foo);
+      let promise;
+      before(() => {
+        promise = helper._validate(mockInputContext);
+      });
+
+      it('should reject with error', () => {
+        return expect(promise).to.eventually
+          .be.rejectedWith(Error, 'missing/incorrect \'id\' String value of ObjectID in job payload');
+
+      })
+    })
+
+  })
 });
